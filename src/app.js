22388d5deac0a512021aee069ba089ad72ed33ae
LaneLR <laneleerichardson@gmail.com>
2025-05-19 09:39:04 -0500

    created tests for GET routes for /musicians, /musicians/1, /bands in app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,41 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
+const { Musician, Band } = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
 
 app.get("/musicians", async (req, res) => {
     const musicians = await Musician.findAll();
+
+    if (res.statusCode !== 200) {
+        throw new Error("Could not access endpoint.")
+    }
+
     res.json(musicians)
 })
 
+app.get("/musicians/1", async (req, res) => {
+    const musicians = await Musician.findByPk(1);
+
+    if (res.statusCode !== 200) {
+        throw new Error("Could not access endpoint.")
+    }
+
+    res.json(musicians)
+})
+
+app.get("/bands", async (req, res) => {
+    const bands = await Band.findAll();
+
+    if (res.statusCode !== 200) {
+        throw new Error("Could not access endpoint.")
+    }
+
+    
+
+    res.json(bands)
+})
 
 
 
